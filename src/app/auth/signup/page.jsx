ff316ac4975d56c8f2ae9445dc8f0969dbd58fa3
codeHasib <mohammadhasib9827@gmail.com>
2026-04-22 19:35:07 +0600

    add navbar with responsiveness

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,11 @@
+const SignUpPage = () => {
+  return (
+    <div>
+      <h2 className="text-center font-extralight text-4xl py-5 underline">
+        Sign Up
+      </h2>
+    </div>
+  );
+};
+
+export default SignUpPage;
